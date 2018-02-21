cd73f9473bb0d9e5317de27dd8de00a15416f65e
Jesse Shiao <jesse850330@gmail.com>
2018-02-22 12:52:36 +1300

    add

--- projects/src/components/Jessse/Jesse.vue
+++ projects/src/components/Jessse/Jesse.vue
@@ -1,13 +1,55 @@
 <template>
-  <div>
-    This is the About page.
-  </div>
+    <div class="Log-page">
+      <h2>Log In</h2>
+      <p>Log in to your account</p>
+      <div class="alert alert-danger" v-if="error">
+        <p>{{ error }}</p>
+      </div>
+      <div class="form-group">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Enter your username"
+          v-model="credentials.username"
+        >
+      </div>
+      <div class="form-group">
+        <input
+          type="password"
+          class="form-control"
+          placeholder="Enter your password"
+          v-model="credentials.password"
+        >
+      </div>
+      <button class="btn btn-primary" @click="submit()">Access</button>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'about'
-}
+  data() {
+    return {
+      // We need to initialize the component with any
+      // properties that will be used in it
+      credentials: {
+        username: "",
+        password: ""
+      },
+      error: ""
+    };
+  },
+  methods: {
+    submit() {
+      var credentials = {
+        username: this.credentials.username,
+        password: this.credentials.password
+      };
+      // We need to pass the component's this context
+      // to properly make use of http in the auth service
+      auth.login(this, credentials, "secretquote");
+    }
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
